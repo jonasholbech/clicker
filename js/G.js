b2cbb3b35d47a444323d5289e959271309b6dda3
Jonas Holbech <jh@jonasholbech.dk>
2016-03-25 16:27:45 +0100

    statusbar gone

--- js/G.js
+++ js/G.js
@@ -4,6 +4,16 @@ var Game = {
     stage:null,
     init:function(){
 
+        var statusBarSize = 0;//22;
+        var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+        var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0) - statusBarSize;
+        var canvas = document.querySelector('canvas');
+
+        canvas.width=w;
+        canvas.height=h;
+        w = canvas.offsetWidth;
+        h = canvas.offsetHeight;
+
         Game.stage = new createjs.Stage("canvas");
         createjs.Touch.enable(Game.stage);
         Preload.load();
